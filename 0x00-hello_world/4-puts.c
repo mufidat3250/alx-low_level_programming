98d1f84450ebf6da0b0ca14c6b7c285dacbc89ff
wahab mufidat <wahabmufidat1919@gmail.com>
2022-09-11 00:24:15 -0700

    fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #!/bin/bash
 #include<stdio.h>
-int main()
+/** 
+ *main - Entry point
+ *
+ * ReturnValue: zero Always
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle")
-		return 0
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
