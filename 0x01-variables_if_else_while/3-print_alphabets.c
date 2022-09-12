69d8ef031be7bc05be6fd378b1fcec6dca660d8f
wahab mufidat <wahabmufidat1919@gmail.com>
2022-09-12 13:53:19 -0700

    print small and capital letters with new lines

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet in lowercase uppercase and follow by new line
+ *
+ * Return: value 0
+ */
+
+inti main(void)
+{
+	int result
+
+	for (result = 'a'; result <= 'z'; result++)
+		putchar(result);
+	for (result = 'A'; result <= 'Z'; result++)
+		putchar(result);
+
+	putchar("\n")
+		return (0);
+
+
+}
