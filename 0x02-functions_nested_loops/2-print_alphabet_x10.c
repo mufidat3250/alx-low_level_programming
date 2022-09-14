f157c176d50871aac1426edbecf1cfcd06a63f10
mufidat3250 <wahabmufidat1919@gmail.com>
2022-09-14 14:00:33 -0700

    fixes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char letter
+	
+	for (int i = 0; i < 10; i++)
+	{
+		for(letter = 'a'; letter <= 'z'; j++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n')
+	}
+}
