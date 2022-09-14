ac3e7a35c335b992bf0ac35ee98c8bd0bf7d2a58
wahab mufidat <wahabmufidat1919@gmail.com>
2022-09-14 12:34:56 -0700

    fixes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,14 @@
 
 int main(void)
 {
-	putchar('');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int str[] = {95, 112, 117, 116, 99, 104, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for(count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
 	_putchar('\n');
 	return (0);
-	
 }
