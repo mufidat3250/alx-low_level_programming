b5ce9946ac57285c88654ea20a980c1d9b714068
mufidat3250 <wahabmufidat1919@gmail.com>
2022-09-15 11:42:38 -0700

    fixes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,10 @@ void more_numbers(void)
 	{
 		for (int j = 0; j <= 14; j++)
 		{
-			_putchar((j % 14), '0');
+			if (j > 9)
+				_putchar((j % 10), '0');
+			_putchar((j % 10) + '0');
 		}
-		_putchar((i % 14), '0')
+		_putchar('\n');
 	}
-	_putchar('\n')
 }
