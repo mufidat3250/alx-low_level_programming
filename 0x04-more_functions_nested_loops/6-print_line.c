a1705804d8bae8e56275457cd8c6825db33ac5eb
mufidat3250 <wahabmufidat1919@gmail.com>
2022-09-15 15:05:24 -0700

    fixes

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,16 +5,11 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (int i = 0; i <= n; i++)
-			putchar('-');
+		for (int i = 0; i < n; i++)
+			_putchar('-');
 	}
 	_putchar('\n');
-	
 }
 
