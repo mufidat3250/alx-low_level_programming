6766c4337e6a5abc2340d19a4f9de327f9751ad2
mufidat3250 <wahabmufidat1919@gmail.com>
2022-09-15 09:36:47 -0700

    fixes

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print numbers from zero to nine
+ * @void: no parameter to check
+ *
+ * Return : return value is void
+ */
+
+void print_numbers(void)
+{
+	for (int num = 0; num <= 9; num++)
+	{
+		_putchar(num)
+	}
+}
