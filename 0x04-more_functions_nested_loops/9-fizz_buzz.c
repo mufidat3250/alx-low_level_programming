8863e088c9d713ee61705ac95f960c791ff82566
mufidat3250 <wahabmufidat1919@gmail.com>
2022-09-16 19:45:07 -0700

    fixes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int num = 100, i;
 
-	for (i = 0; i < num; i++)
+	for (i = 1; i <= num; i++)
 	{
 		if (i % 3 == 0)
 		{
@@ -20,8 +20,13 @@ int main(void)
 		}
 		else if (i % 15 == 0)
 		{
-			printf('FizzBuzz')
+			printf('FizzBuzz');
 		}
+		else
+		{
+			printf("%d", num)
+		}
+		printf("\n")
 	}
 	return (0)
 }
