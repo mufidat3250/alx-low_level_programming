134097095afa6803511bbf09b63d3c2b487fd463
mufidat3250 <wahabmufidat1919@gmail.com>
2022-09-16 20:06:10 -0700

    fixes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,22 +10,20 @@ int main(void)
 
 	for (i = 1; i <= num; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-		printf("Fizz\n");
+			printf("Fizz Buss\n")
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz\n");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz\n");
-		}
-		else if (i % 15 == 0)
-		{
-			printf("Fizz Buzz\n");
+			printf("Buss\n");
 		}
 		else
-		{
-			printf("%d\n", num)
-		}
+			printf("%d\n", num);
 		printf("\n")
 	}
 	return (0)
