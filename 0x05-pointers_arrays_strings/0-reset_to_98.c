d9dd1d2b35b04e0e4f04da3910121f7c6dbd9773
mufidat3250 <wahabmufidat1919@gmail.com>
2022-09-20 13:30:07 -0700

    fixes

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * Reset_to_98 - function that takes a pointer to an int and update it
+ * reset_to_98 - function that takes a pointer to an int and update it
  * @*n: function parameter
+ *
  * Return: void
  */
 
