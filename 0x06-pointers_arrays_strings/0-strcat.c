734be872a42d7f0fb81781dedb2acc2917e8a67e
mufidat3250 <wahabmufidat1919@gmail.com>
2022-09-21 12:56:27 -0700

    fixes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,18 +10,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, i;
+	int len = 0, i = 0;
 
-	while (dest[len])
+	while (*(dest + len))
+		i++;
+	while ((*(dest + len) = *(src + i)))
 	{
 		len++;
+		i++;
 	}
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[len] = src[i];
-		len++;
-	}
-	dest[len] = '\0';
-
 	return (dest);
 }
